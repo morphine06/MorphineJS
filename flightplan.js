7227a6bec1ec0565ea49dcafee1d1eff7f88f37a
morphine06 <david@miglior.fr>
2018-01-15 11:35:41 +0100

    flightplan ok

---
+++ flightplan.js
@@ -0,0 +1,23 @@
+var plan = require("flightplan");
+plan.target("dev", {
+	host: "www.wywiwyg.net",
+	username: "root",
+	agent: process.env.SSH_AUTH_SOCK
+});
+// var commentgit = (process.argv[3])?process.argv[3]:'automatic commit' ;
+
+plan.local("publish", function(local) {
+	local.exec("git add .");
+	var input = local.prompt("Commit message ?");
+	if (input === "") plan.abort("Commit message mandatory");
+	local.exec('git commit -m "' + input + '"');
+	local.exec("git push");
+});
+
+plan.remote("publish", function(remote) {
+	remote.with("cd /home/MorphineJS", function() {
+		remote.exec("git pull");
+		// remote.exec("npm install");
+		remote.exec("supervisorctl restart morphinejs");
+	});
+});
